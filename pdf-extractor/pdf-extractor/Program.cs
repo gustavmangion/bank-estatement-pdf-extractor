52b5a54bf31b29a68f450ca0f3d6c05859239aaf
Gustav <gustavmangion@hotmail.com>
2023-07-21 12:04:35 +0400

    Get PDf password from txt file

--- pdf-extractor/pdf-extractor/Program.cs
+++ pdf-extractor/pdf-extractor/Program.cs
@@ -2,8 +2,16 @@
 
 Console.WriteLine("ADCB Bank E-Statement PDF Extractor");
 
+string pdfPassword = getPDFPassword("statement_pwd.txt");
+
 string path = Path.Combine(Directory.GetCurrentDirectory(), "AccountStatement.pdf");
-using (PdfDocument document = PdfDocument.Open(path))
+using (PdfDocument document = PdfDocument.Open(path, new ParsingOptions { Password = pdfPassword }))
 {
     int pageCount = document.NumberOfPages;
+}
+
+
+string getPDFPassword(string filename)
+{
+    return File.ReadAllText(filename);
 }
