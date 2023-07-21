a3368608074bcea9b4ac867e5fd707f4e412e0d1
Gustav <gustavmangion@hotmail.com>
2023-07-21 14:26:33 +0400

    Add regex for removing text inserted on every new page

--- pdf-extractor/pdf-extractor/Program.cs
+++ pdf-extractor/pdf-extractor/Program.cs
@@ -1,15 +1,38 @@
-﻿using UglyToad.PdfPig;
+﻿using System.Text.RegularExpressions;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
 
 Console.WriteLine("ADCB Bank E-Statement PDF Extractor");
 
 string pdfPassword = getPDFPassword("statement_pwd.txt");
 
 string path = Path.Combine(Directory.GetCurrentDirectory(), "AccountStatement.pdf");
+string pageContent = "";
+
 using (PdfDocument document = PdfDocument.Open(path, new ParsingOptions { Password = pdfPassword }))
 {
     int pageCount = document.NumberOfPages;
+
+    for(int i = 1; i < pageCount; i++)
+    {
+        //Page numbering is 1 indexed
+        Page page = document.GetPage(i+1);
+
+        pageContent += page.Text;
+    }
 }
 
+pageContent= cleanNextPageEntities(pageContent);
+Console.WriteLine(pageContent);
+
+string cleanNextPageEntities(string text)
+{
+    string pattern = "(Page [0-9] of [0-9]Transactions Details for the period from " +
+        "[0-9]{2}/[0-9]{2}/[0-9]{4} to [0-9]{2}/[0-9]{2}/[0-9]{4}DateDescriptionChq" +
+        "/Ref No.Value DateDebitCreditBalance)";
+
+    return Regex.Replace(text, pattern, "");
+}
 
 string getPDFPassword(string filename)
 {
