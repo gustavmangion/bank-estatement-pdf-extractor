63a60b1c396266468ac430cf8927d7a8e2b6fe40
Gustav <gustavmangion@hotmail.com>
2023-07-21 12:02:30 +0400

    Add logic to open encrypted PDF

--- pdf-extractor/pdf-extractor/Program.cs
+++ pdf-extractor/pdf-extractor/Program.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine("ADCB Bank E-Statement PDF Extractor");
+﻿using UglyToad.PdfPig;
 
+Console.WriteLine("ADCB Bank E-Statement PDF Extractor");
 
+string path = Path.Combine(Directory.GetCurrentDirectory(), "AccountStatement.pdf");
+using (PdfDocument document = PdfDocument.Open(path))
+{
+    int pageCount = document.NumberOfPages;
+}
