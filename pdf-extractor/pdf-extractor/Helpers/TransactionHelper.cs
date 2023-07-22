23b559f333fde7bd20f8c760eefb568630473b8d
Gustav <gustavmangion@hotmail.com>
2023-07-22 12:55:30 +0400

    Remove unused imports for helper

--- pdf-extractor/pdf-extractor/Helpers/TransactionHelper.cs
+++ pdf-extractor/pdf-extractor/Helpers/TransactionHelper.cs
@@ -1,11 +1,5 @@
 ﻿using pdf_extractor.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace pdf_extractor.Helpers
 {
